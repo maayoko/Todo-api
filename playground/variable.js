8af9da5d051e95181103865e2bbb9a38a90216bc
Ivan Ivicek <ivicek.ivan@gmail.com>
2016-01-26 23:08:45 +0100

    Add sequelize demo

---
+++ playground/variable.js
@@ -0,0 +1,28 @@
+// var person = {
+//     name: "Ivan",
+//     age: 21
+// }
+// 
+// function updatePerson(obj) {
+//     obj = {
+//         name: "Ivan",
+//         age: 24
+//     };
+//     console.log(obj);
+// }
+// 
+// updatePerson(person);
+// console.log(person);
+
+
+var grades = [17,37]
+function addGrade(arr) {
+    // arr = [14,28];
+    arr.push(19);
+    // console.log(arr);
+    debugger;
+}
+
+addGrade(grades);
+
+console.log(grades);
