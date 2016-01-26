21cdc462102721ed639b97bc6aac44bbb77e83eb
Ivan Ivicek <ivicek.ivan@gmail.com>
2016-01-26 16:08:00 +0100

    Add PUT todos/:id

--- server.js
+++ server.js
@@ -47,11 +47,37 @@ app.delete("/todos/:id", function(req, res) {
     var matchedTodo = _.findWhere(todos, {id: todoId});
     
     if(!matchedTodo) {
-        res.status(404).send();
+        res.status(404).send({"error": "no todo found with that id"});
     } else {
         todos = _.without(todos, matchedTodo);
         res.json(matchedTodo);
     }
+})
+
+app.put("/todos/:id", function(req, res) {
+    var todoId = parseInt(req.params.id);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var body = _.pick(req.body, "description", "completed");
+    var validAttributes = {};
+    
+    if(!matchedTodo) {
+        return res.status(404).send({"error": "no todo found with that id"});
+    }
+    
+    if(body.hasOwnProperty("completed") && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if(body.hasOwnProperty("completed")) {
+        return res.status(400).send();
+    }
+    
+    if(body.hasOwnProperty("description") && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if(body.hasOwnProperty("description")) {
+        return res.status(400).send();
+    }
+    
+    _.extend(matchedTodo, validAttributes);
+    res.json(validAttributes);
 })
 
 app.listen(port, function() {
