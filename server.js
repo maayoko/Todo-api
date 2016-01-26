d6daf331fac5b7901b32e515e587258cfe125a7d
Ivan Ivicek <ivicek.ivan@gmail.com>
2016-01-26 14:01:15 +0100

    Add DELETE todos route

--- server.js
+++ server.js
@@ -42,6 +42,18 @@ app.post("/todos", function(req, res) {
     todoNextId++;
 });
 
+app.delete("/todos/:id", function(req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    
+    if(!matchedTodo) {
+        res.status(404).send();
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
+})
+
 app.listen(port, function() {
     console.log("Express listening on port: " + port + "!");
 })
